10622078b8f404f8d0d264a6f93d767585567ec6
coderGirlSu <95465740+coderGirlSu@users.noreply.github.com>
2022-11-16 19:31:53 +1100

    updated domain name

--- src/server.js
+++ src/server.js
@@ -15,9 +15,9 @@ require('dotenv').config()
 let httpsServer
 // Certificate
 if (process.env.USE_HTTPS == "true") {
-    const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-    const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-    const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/tiktalk-server.codergirlsu.dev/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/tiktalk-server.codergirlsu.dev/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/tiktalk-server.codergirlsu.dev/chain.pem', 'utf8');
 
     const credentials = {
         key: privateKey,
