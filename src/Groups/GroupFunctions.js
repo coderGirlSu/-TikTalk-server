6dd3ff7a1c2b7d90f378830b5d53d075f8797d20
coderGirlSu <95465740+coderGirlSu@users.noreply.github.com>
2022-11-17 23:10:45 +1100

    uncommented leave group

--- src/Groups/GroupFunctions.js
+++ src/Groups/GroupFunctions.js
@@ -25,6 +25,57 @@ async function getUserGroup(userDetails){
     return userGroups
 }
 
+// add user to a group
+async function addUserToGroup(groupDetails){
+
+    // use friend's email from firebase to get the user id
+    let userRecord = await firebaseAdmin.auth().getUserByEmail(groupDetails.userEmail)
+
+    // find the group I want add the uer to from mongoDB
+    let userGroup = await Group.findById(groupDetails.groupId) 
+    
+    // check if user is already in the group
+   if (!userGroup.userIds.includes(userRecord.uid)){
+    userGroup.userIds.push(userRecord.uid)
+    await userGroup.save()
+    return userGroup
+   }else {
+    return {"error":'you can\'t do this! user is already in this group'}
+   }
+     
+    // .then((getUsersResult) => {
+    //   console.log('Successfully fetched user data:');
+    //   getUsersResult.users.forEach((userRecord) => {
+    //     console.log(userRecord);
+    //   });
+  
+    //   console.log('Unable to find users corresponding to these identifiers:');
+    //   getUsersResult.notFound.forEach((userIdentifier) => {
+    //     console.log(userIdentifier);
+    //   });
+    // })
+    // .catch((error) => {
+    //   console.log('Error fetching user data:', error);
+    // });
+}
+
+async function leaveGroup(userDetails){
+    console.log(userDetails)
+    let userGroup = await Message.findById({groupId:userDetails.groupId})
+
+    // console.log(userGroup)
+
+    let userRecord = await firebaseAdmin.auth().getUsers(userDetails.userId)
+
+    console.log(userRecord)
+
+    // let Result = await Group.findByIdAndDelete(userRecord.uid)
+
+    return Result
+}
+
+
+
 module.exports = {
     getHistory,
     createGroup,
